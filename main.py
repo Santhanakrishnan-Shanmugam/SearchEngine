53e14bed4bdb362edfc7aedf8194326d6d799b53
Joshua97-josh <joshuaofficial97@gmail.com>
2025-09-29 08:15:40 +0530

    done

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ from fastapi.middleware.cors import CORSMiddleware
 from chain import RAG
 
 app = FastAPI()
+
+
 origins = [
     "http://neurasearch.s3-website.ap-south-1.amazonaws.com",
 ]
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,   
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,       
+    allow_credentials=True,      
+    allow_methods=["*"],         
+    allow_headers=["*"],         
 )
+
+@app.options("/")
+async def options_root():
+    return {}
 
 @app.post("/")
 async def query_endpoint(request: Request):
     try:
-        
         data = await request.json()
-        print("Raw JSON received:", data)
-
-        
         if "query" not in data:
             return {"documents": [], "all_documents": [], "llm_answer": "Missing 'query' in request"}
-
-        
         result = RAG({"query": data["query"]})
         return result
     except Exception as e:
