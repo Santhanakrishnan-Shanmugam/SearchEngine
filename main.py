192e896cc8657241d2c2ee812ce1a94de7c103ef
Joshua97-josh <joshuaofficial97@gmail.com>
2025-09-29 07:40:04 +0530

    DONE

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ origins = [
 ]
 
 
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 
 class QueryRequest(BaseModel):
     query: str
