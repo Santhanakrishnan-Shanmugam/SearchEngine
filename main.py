a6a6ca883a6f44f8ea54f4ced25fcbf1c9e8db0e
Joshua97-josh <joshuaofficial97@gmail.com>
2025-09-30 14:25:25 +0530

    DONE

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from chain import RAG
-import os
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://neura-search7.onrender.com", "*"],
+    allow_origins=["*"],  # temporary for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,7 +23,3 @@ async def search(request: QueryRequest):
     return result
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000)) 
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port)
